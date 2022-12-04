0ac2e1b1b0414dfca3d6f298fb0ecce12bc3e5a2
Rémi Côme <remi.come@odit-e.com>
2022-12-04 06:42:33 +0100

    Day 4, part 2

--- 2022/04/__main__.py
+++ 2022/04/__main__.py
@@ -32,8 +32,18 @@ def is_redundant(assignment: Assignment) -> bool:
     return assignment.left <= assignment.right or assignment.right <= assignment.left
 
 
+def overlaps(assignment: Assignment) -> bool:
+    """Return True if one elf's asignment intersects the other."""
+    return bool(assignment.left & assignment.right)
+
+
 if __name__ == "__main__":
     filepath = os.path.join(os.path.dirname(__file__), "input")
+
     assignments = (Assignment.from_line(line) for line in lines(filepath))
     reduncancies = (is_redundant(assignment) for assignment in assignments)
     print(f"Redundant assignments: {sum(reduncancies)}")
+
+    assignments = (Assignment.from_line(line) for line in lines(filepath))
+    overlapping = (overlaps(assignment) for assignment in assignments)
+    print(f"Overlapping assignments: {sum(overlapping)}")
