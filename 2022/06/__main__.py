c1e250265962213746be2f5b3c99b922b4830720
Rémi Côme <remi.come@odit-e.com>
2022-12-06 11:54:33 +0100

    Day 6, part 2

--- 2022/06/__main__.py
+++ 2022/06/__main__.py
@@ -1,8 +1,6 @@
 import os
 import typing
 
-filepath = "input"
-
 
 def stream(filepath: str) -> typing.Iterable[str]:
     """Stream characters from given file"""
@@ -11,15 +9,15 @@ def stream(filepath: str) -> typing.Iterable[str]:
             yield character
 
 
-def is_packet_start(codon: str) -> bool:
-    return len(set(codon)) == 4
+def is_marker(codon: str, marker_length: int = 4) -> bool:
+    return len(set(codon)) == marker_length
 
 
-def index_of_earliest_packet(filepath) -> int:
+def index_of_earliest_marker(filepath, marker_length: int = 4) -> int:
     buffer = ""
     for i, character in enumerate(stream(filepath)):
-        buffer = (buffer + character)[-4:]
-        if is_packet_start(buffer):
+        buffer = (buffer + character)[-marker_length:]
+        if is_marker(buffer, marker_length=marker_length):
             return i + 1
 
     raise ValueError
@@ -27,4 +25,9 @@ def index_of_earliest_packet(filepath) -> int:
 
 if __name__ == "__main__":
     filepath = os.path.join(os.path.dirname(__file__), "input")
-    print(f"Earliest packet received at: {index_of_earliest_packet(filepath)}")
+
+    earliest_packet = index_of_earliest_marker(filepath, marker_length=4)
+    print(f"Earliest packet received at: {earliest_packet}")
+
+    earliest_message = index_of_earliest_marker(filepath, marker_length=14)
+    print(f"Earliest message received at: {earliest_message}")
