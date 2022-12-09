fad46b258038ff23b3cd5664b95f27db9643284a
Rémi Côme <remi.come@odit-e.com>
2022-12-09 09:50:48 +0100

    Day 9, part 2

--- 2022/09/__main__.py
+++ 2022/09/__main__.py
@@ -67,32 +67,52 @@ class Bridge:
     """Head and tail coordinates."""
 
     head: Coordinates = dataclasses.field(default_factory=Coordinates)
-    tail: Coordinates = dataclasses.field(default_factory=Coordinates)
+    knots: typing.List[Coordinates] = dataclasses.field(
+        default_factory=lambda: [Coordinates()]
+    )
+
+    def __post_init__(self):
+        if self.knots is None:
+            self.knots = [Coordinates()]
+
+    @property
+    def tail(self) -> Coordinates:
+        return self.knots[-1]
 
     def follow(self, head) -> "Bridge":
         """Return an updated copy of the bridge tail following the head."""
-        relative = head - self.tail
 
-        if abs(relative.x) <= 1 and abs(relative.y) <= 1:
-            # Head and self.tail are touching
-            offset = Coordinates(0, 0)
-        elif relative.x == 0:
-            offset = Coordinates(y=1 if relative.y > 0 else -1)
-        elif relative.y == 0:
-            offset = Coordinates(x=1 if relative.x > 0 else -1)
-        else:
-            # Diagonal move
-            offset = Coordinates(
-                x=1 if relative.x > 0 else -1,
-                y=1 if relative.y > 0 else -1,
-            )
-        return Bridge(head=head, tail=self.tail + offset)
+        def move_knot(knot, ahead_knot) -> Coordinates:
+            relative = ahead_knot - knot
+
+            if abs(relative.x) <= 1 and abs(relative.y) <= 1:
+                # Knots are touching
+                offset = Coordinates(0, 0)
+            elif relative.x == 0:
+                offset = Coordinates(y=1 if relative.y > 0 else -1)
+            elif relative.y == 0:
+                offset = Coordinates(x=1 if relative.x > 0 else -1)
+            else:
+                # Diagonal move
+                offset = Coordinates(
+                    x=1 if relative.x > 0 else -1,
+                    y=1 if relative.y > 0 else -1,
+                )
+            return knot + offset
+
+        ahead_knot = head
+        knots = []
+        for knot in self.knots:
+            ahead_knot = move_knot(knot, ahead_knot)
+            knots.append(ahead_knot)
+
+        return Bridge(head=head, knots=knots)
 
 
-def bridges(filepath) -> typing.Iterable[Bridge]:
+def bridges(filepath, number_of_knots=1) -> typing.Iterable[Bridge]:
     moves = (Move.from_line(line) for line in lines(filepath))
 
-    bridge = Bridge()
+    bridge = Bridge(knots=[Coordinates() for _ in range(number_of_knots)])
     yield bridge
     for move in moves:
         for head in move.apply(bridge.head):
@@ -105,3 +125,11 @@ if __name__ == "__main__":
 
     tail_positions = {(bridge.tail.x, bridge.tail.y) for bridge in bridges(filepath)}
     print(f"Different positions: {len(tail_positions)}")
+
+    tail_positions = {
+        (bridge.tail.x, bridge.tail.y)
+        for bridge in bridges(filepath, number_of_knots=9)
+    }
+    print(f"Different positions with 10 knots: {len(tail_positions)}")
+
+    filepath = os.path.join(os.path.dirname(__file__), "test_input")
