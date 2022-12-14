3044168daf5f0a389c8b0fbb6da53c34fd98758a
Rémi Côme <remi.come@odit-e.com>
2022-12-14 08:56:45 +0100

    Simplify lines generator

--- 2022/common.py
+++ 2022/common.py
@@ -4,8 +4,5 @@ import typing
 def lines(filepath: str) -> typing.Iterable[str]:
     """Stream lines from given file."""
     with open(filepath, "r") as f:
-        while True:
-            line = f.readline()
-            if line == "":
-                break
+        while line := f.readline():
             yield line
