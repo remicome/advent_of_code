0eb7d7c5480c44e7d8da83687e140f18269db168
Rémi Côme <remi.come@odit-e.com>
2022-12-15 11:16:26 +0100

    Day 15, part 2

--- 2022/15/__main__.py
+++ 2022/15/__main__.py
@@ -1,7 +1,10 @@
 import dataclasses
+import itertools
 import os
 import re
 import typing
+
+from tqdm import tqdm
 
 from ..common import Point, lines
 
@@ -31,6 +34,16 @@ class Interval:
 
     def __len__(self) -> int:
         return self.stop - self.start + 1
+
+    def __and__(self, other: "Interval") -> "Interval":
+        """Intersect with other closed interval"""
+        return Interval(
+            start=max(self.start, other.start),
+            stop=min(self.stop, other.stop),
+        )
+
+    def __iter__(self) -> typing.Iterable[int]:
+        yield from range(self.start, self.stop + 1)
 
     @property
     def empty(self) -> bool:
@@ -86,7 +99,7 @@ def non_intersecting(intervals: typing.Iterable[Interval]) -> typing.List[Interv
     return [lowest_interval] + non_intersecting(updated_intervals)
 
 
-def numbered_of_empty_positions(filepath, y: int = 2000000) -> int:
+def number_of_empty_positions(filepath, y: int = 2000000) -> int:
     spheres = (
         ManhattanSphere(center=sensor, radius=manhattan_distance(sensor, beacon))
         for sensor, beacon in sensors_and_beacons(filepath)
@@ -104,9 +117,43 @@ def numbered_of_empty_positions(filepath, y: int = 2000000) -> int:
     return size(non_empty) - len(beacons_on_row)
 
 
+def beacon_position(filepath, bound: Interval = Interval(0, 4000000)) -> Point:
+    """Find the distress beacon's position."""
+    spheres = [
+        ManhattanSphere(center=sensor, radius=manhattan_distance(sensor, beacon))
+        for sensor, beacon in sensors_and_beacons(filepath)
+    ]
+
+    for y in tqdm(bound, total=len(bound)):
+        row_intersections = (sphere.row_intersection(y=y) & bound for sphere in spheres)
+        coverage = (
+            intersection for intersection in row_intersections if not intersection.empty
+        )
+        # Order by minimal left bound, then iterate to find a gap between to
+        # successive coverage intervals
+        coverage = sorted(coverage, key=lambda interval: interval.start)
+
+        if len(coverage) == 1 and coverage[0] != bound:
+            raise NotImplementedError
+
+        rightmost_stop = bound.start
+        for interval in coverage:
+            if rightmost_stop + 1 < interval.start:
+                # We found the gap!
+                return Point(rightmost_stop + 1, y)
+            rightmost_stop = max(rightmost_stop, interval.stop)
+
+    raise ValueError("No beacon found")
+
+
 if __name__ == "__main__":
     filepath = os.path.join(os.path.dirname(__file__), "input")
 
     y = 2000000
-    n_empty = numbered_of_empty_positions(filepath, y=y)
+    n_empty = number_of_empty_positions(filepath, y=y)
     print(f"Number of excluded positions at row {y}: {n_empty}")
+
+    bound = Interval(0, 4000000)
+    position = beacon_position(filepath, bound=bound)
+    tuning_frequency = position.x * bound.stop + position.y
+    print(f"Tuning frequency of the distress signal: {tuning_frequency}")
