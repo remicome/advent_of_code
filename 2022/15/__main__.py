067e2767c0e23b97766758d1d1a386c5209f9279
Rémi Côme <remi.come@odit-e.com>
2022-12-15 10:23:22 +0100

    Day 15, part 1

--- 2022/15/__main__.py
+++ 2022/15/__main__.py
@@ -6,27 +6,6 @@ import typing
 from ..common import Point, lines
 
 
-@dataclasses.dataclass
-class ManhattanSphere:
-    center: int
-    radius: int
-
-    def row_intersection(self, y: int) -> typing.Set[Point]:
-        """Intersect the sphere with the row at position y."""
-        points_on_each_side = self.radius - abs(self.center.y - y)
-        if points_on_each_side < 0:
-            return set()
-        else:
-            return {
-                Point(self.center.x + x, y) for x in range(points_on_each_side + 1)
-            } | {Point(self.center.x - x, y) for x in range(1, points_on_each_side + 1)}
-
-
-def manhattan_distance(left: Point, right: Point) -> int:
-    difference = left - right
-    return abs(difference.x) + abs(difference.y)
-
-
 def sensors_and_beacons(filepath) -> typing.Iterable[tuple]:
     """Sensors and beacon pairs."""
     pattern = r".*x=([-\d]+), y=([-\d]+):.*x=([-\d]+), y=([-\d]+)\n"
@@ -38,18 +17,91 @@ def sensors_and_beacons(filepath) -> typing.Iterable[tuple]:
         )
 
 
+def manhattan_distance(left: Point, right: Point) -> int:
+    difference = left - right
+    return abs(difference.x) + abs(difference.y)
+
+
+@dataclasses.dataclass
+class Interval:
+    """Closed interval representation."""
+
+    start: int
+    stop: int
+
+    def __len__(self) -> int:
+        return self.stop - self.start + 1
+
+    @property
+    def empty(self) -> bool:
+        return self.start > self.stop
+
+
+@dataclasses.dataclass
+class ManhattanSphere:
+    center: int
+    radius: int
+
+    def row_intersection(self, y: int) -> Interval:
+        """Intersect the sphere with the row at position y."""
+        points_on_each_side = self.radius - abs(self.center.y - y)
+        return Interval(
+            start=self.center.x - points_on_each_side,
+            stop=self.center.x + points_on_each_side,
+        )
+
+
+def size(intervals: typing.Iterable[Interval]) -> int:
+    """Size a collection of intervals."""
+    return sum(len(interval) for interval in non_intersecting(intervals))
+
+
+def non_intersecting(intervals: typing.Iterable[Interval]) -> typing.List[Interval]:
+    """
+    Convert an iterable of ranges into an equivalent iterable of non-intersecting
+    ranges.
+    """
+    interval_list = list(intervals)
+    if len(interval_list) <= 1:
+        return interval_list
+
+    lowest_interval_index = min(
+        range(len(interval_list)), key=lambda i: interval_list[i].start
+    )
+    lowest_interval = interval_list.pop(lowest_interval_index)
+
+    # Reindex all other ranges so that they don't intersect this one
+    def difference(interval: Interval, lowest_interval=lowest_interval) -> Interval:
+        return Interval(
+            start=max(1 + lowest_interval.stop, interval.start),
+            stop=interval.stop,
+        )
+
+    updated_intervals = (
+        difference(interval, lowest_interval) for interval in interval_list
+    )
+    updated_intervals = (
+        interval for interval in updated_intervals if not interval.empty
+    )
+    return [lowest_interval] + non_intersecting(updated_intervals)
+
+
 def numbered_of_empty_positions(filepath, y: int = 2000000) -> int:
     spheres = (
         ManhattanSphere(center=sensor, radius=manhattan_distance(sensor, beacon))
         for sensor, beacon in sensors_and_beacons(filepath)
     )
     row_intersections = (sphere.row_intersection(y=y) for sphere in spheres)
-    non_empty = (intersection for intersection in row_intersections if intersection)
-    covered_positions = set.union(*non_empty)
+    non_empty = (
+        intersection for intersection in row_intersections if not intersection.empty
+    )
 
-    # Return beacons at this row
-    beacons = {beacon for _, beacon in sensors_and_beacons(filepath) if beacon.y == y}
-    return len(covered_positions - beacons)
+    # Return beacons at this row: all beacons here have been detected by a sensor, thus is
+    # on one of the spheres
+    beacons_on_row = {
+        beacon for _, beacon in sensors_and_beacons(filepath) if beacon.y == y
+    }
+    return size(non_empty) - len(beacons_on_row)
 
 
 if __name__ == "__main__":
